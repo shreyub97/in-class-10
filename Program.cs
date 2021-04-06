4c44311b7eba53f4777d95675084b3c2b76ea8b6
Shreya Uday <shreyauday@usf.edu>
2021-04-06 08:17:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace In_class_10
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+
+        public List<OrderDetails> orderPlaced { get; set; }
+
+    }
+
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<OrderDetails> productPurchasal { get; set; }
+    }
+
+    class OrderDetails
+    {
+        public int Id { get; set; }
+        public Order PlacedOrder { get; set; }
+        public Product PurchasedProduct { get; set; }
+
+        public DateTime PurchasedDate { get; set; }
+    }
+
+    class StoreContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<OrderDetails> OrderDetailss { get; set; }
+
+        string connectionString = "Server = (localdb)\\MSSQLlocalDB;Database=ManyManyDemo;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StoreContext context = new StoreContext())
+            {
+                context.Database.EnsureCreated();
+
+                Order order = new Order(OrderName = "Walmart");
+                Product product = new Product(Name = " groceries ");
+                OrderDetails orderDetails = new OrderDetails
+                {
+                    PlacedOrder = order,
+                    PurchasedProduct = product,
+                    PurchasedDate = DateTime.Now
+                };
+
+                context.Orders.Add(order);
+                context.Products.Add(product);
+                context.OrderDetailss.Add(orderDetails);
+
+                context.SaveChanges();
+
+            }
+
+
+        }
+    }
+}
